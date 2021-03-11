4134235c8cebb68413bc3f63be03db18633f7efc
Bapat <sarthak.bapat@iao.fraunhofer.de>
2021-03-11 17:02:21 +0100

    Adding devel directory in catkin_ws

---
+++ catkin_ws/devel/share/gennodejs/ros/rtabmap_ros/msg/_index.js
@@ -0,0 +1,34 @@
+
+"use strict";
+
+let UserData = require('./UserData.js');
+let Point3f = require('./Point3f.js');
+let Goal = require('./Goal.js');
+let MapData = require('./MapData.js');
+let Point2f = require('./Point2f.js');
+let Link = require('./Link.js');
+let Path = require('./Path.js');
+let KeyPoint = require('./KeyPoint.js');
+let OdomInfo = require('./OdomInfo.js');
+let Info = require('./Info.js');
+let GPS = require('./GPS.js');
+let MapGraph = require('./MapGraph.js');
+let RGBDImage = require('./RGBDImage.js');
+let NodeData = require('./NodeData.js');
+
+module.exports = {
+  UserData: UserData,
+  Point3f: Point3f,
+  Goal: Goal,
+  MapData: MapData,
+  Point2f: Point2f,
+  Link: Link,
+  Path: Path,
+  KeyPoint: KeyPoint,
+  OdomInfo: OdomInfo,
+  Info: Info,
+  GPS: GPS,
+  MapGraph: MapGraph,
+  RGBDImage: RGBDImage,
+  NodeData: NodeData,
+};
